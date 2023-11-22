17a2308753ea9d5c7982aa7f0795bde83c94f286
edriso <edriso.dev@gmail.com>
2023-11-22 23:05:18 +0200

    add products page design

--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -1,10 +1,52 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { HeaderLayout, SingleProduct } from '../components';
+import { products } from '../utils/products';
+
 function Products() {
+  const navigate = useNavigate();
+  const [checkedProducts, setCheckedProducts] = useState([]);
+
+  const handleCheckboxChange = (productId) => {
+    setCheckedProducts((prevIds) => {
+      if (prevIds.includes(productId)) {
+        return prevIds.filter((id) => id !== productId);
+      } else {
+        return [...prevIds, productId];
+      }
+    });
+  };
+
+  const handleDelete = () => {
+    console.log('Selected Product IDs:', checkedProducts);
+  };
+
   return (
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-    </ul>
+    <main className="products">
+      <HeaderLayout
+        title="Product List"
+        btnOneText="Add"
+        btnTwoText="Mass Delete"
+        btnOneAction={() => navigate('/add-product')}
+        btnTwoAction={handleDelete}
+        btnTwoId="delete-product-btn"
+      />
+
+      <section className="products__product-container">
+        {products.map((product) => {
+          return (
+            <SingleProduct key={product.id} product={product}>
+              <input
+                type="checkbox"
+                name="delete-checkbox"
+                value={product.id}
+                onChange={() => handleCheckboxChange(product.id)}
+              />
+            </SingleProduct>
+          );
+        })}
+      </section>
+    </main>
   );
 }
 
---
+++ client/src/utils/products.js
@@ -0,0 +1,66 @@
+export const products = [
+  {
+    id: 1,
+    sku: 'SKU001',
+    name: 'Product A',
+    price: 19.99,
+    type: 'Book',
+    attribute: {
+      weight: 1.5,
+    },
+  },
+  {
+    id: 2,
+    sku: 'SKU002',
+    name: 'Product B',
+    price: 29.99,
+    type: 'DVD',
+    attribute: {
+      size: 700,
+    },
+  },
+  {
+    id: 3,
+    sku: 'SKU003',
+    name: 'Product C',
+    price: 39.99,
+    type: 'Furniture',
+    attribute: {
+      height: 120,
+      width: 80,
+      length: 180,
+    },
+  },
+  {
+    id: 4,
+    sku: 'SKU004',
+    name: 'Product D',
+    price: 9.99,
+    type: 'Book',
+    attribute: {
+      weight: 0.8,
+    },
+  },
+  {
+    id: 5,
+    sku: 'SKU005',
+    name: 'Product E',
+    price: 49.99,
+    type: 'Furniture',
+    attribute: {
+      height: 150,
+      width: 100,
+      length: 200,
+    },
+  },
+  {
+    id: 6,
+    sku: 'SKU006',
+    name: 'Product F',
+    price: 59.99,
+    type: 'DVD',
+    attribute: {
+      size: 1200,
+    },
+  },
+];
