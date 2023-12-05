ba0c9839ea52c8c94675df672a84e7a72d4df0c5
edriso <edriso.dev@gmail.com>
2023-12-06 00:26:49 +0200

    fix class name

--- Http/Controllers/ProductsController.php
+++ Http/Controllers/ProductsController.php
@@ -4,19 +4,19 @@ namespace Http\Controllers;
 
 use Core\Database;
 
-class ProductPropertiesController
-{
-    public function index()
-    {
+class ProductsController {
+    public function index() {
         $db = new Database();
         $products = $db->query('SELECT id, sku, name, price FROM products')->get();
 
         sendJsonResponse($products);
     }
     
-    public function store()
-    {
+    public function store() {
         echo 'store product';
     }
 
+    public function destroy() {
+        echo 'destroy product';
+    }
 }
