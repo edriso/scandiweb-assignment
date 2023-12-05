74a1148e7495c60ee3e800984e35f726f665587b
edriso <edriso.dev@gmail.com>
2023-12-06 00:27:26 +0200

    change variable name

--- router.php
+++ router.php
@@ -7,8 +7,8 @@ function handleApiRequest($uri, $routes) {
         $routeInfo = $routes[$uri] ?? null;
         if ($routeInfo) {
             $controllerClassName = 'Http\Controllers\\' . $routeInfo['controller'];
-            $method = $routeInfo['method'] ?? 'index';
-            callControllerMethod($controllerClassName, $method);    
+            $action = $routeInfo['action'] ?? 'index';
+            callControllerAction($controllerClassName, $action);    
         } else {
             abort();
         }
@@ -18,43 +18,13 @@ function handleApiRequest($uri, $routes) {
     }
 }
 
-function callControllerMethod($controllerClassName, $method) {
+function callControllerAction($controllerClassName, $action) {
     try {
         $controller = new $controllerClassName();
-        $controller->$method();
+        $controller->$action();
     } catch (\Error) {
         abort('Not found');
     }
-}
-
-function abort(
-    $errorMessage = 'Resource not found', $statusCode = 404
-) {
-    $response = [
-        'error' => [
-            'message' => $errorMessage,
-        ],
-    ];
-    
-    sendJsonResponse($response, $statusCode);
-}
-
-function sendJsonResponse($responseData, $statusCode = 200) {
-    http_response_code($statusCode);
-    header('Content-Type: application/json');
-
-    $response = [
-        'status' => $statusCode,
-    ];
-
-    if (is_array($responseData) && isset($responseData['error'])) {
-        $response['error'] = $responseData['error'];
-    } else {
-        $response['data'] = $responseData;
-    }
-
-    echo json_encode($response);
-    exit;
 }
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
--- routes.php
+++ routes.php
@@ -1,15 +1,8 @@
 <?php
 
 return [
-    '/api/v1/products' => ['controller' => 'ProductsController', 'method' => 'index'],
-    '/api/v1/product' => ['controller' => 'ProductsController', 'method' => 'store'],
-    '/api/v1/product-types' => ['controller' => 'ProductTypesController', 'method' => 'index'],
-    '/api/v1/product-properties' => ['controller' => 'ProductPropertiesController', 'method' => 'show'],
+    '/api/v1/products' => ['controller' => 'ProductsController'],
+    '/api/v1/product' => ['controller' => 'ProductsController', 'action' => 'store'],
+    '/api/v1/product-types' => ['controller' => 'ProductTypesController'],
+    '/api/v1/type-properties' => ['controller' => 'TypePropertiesController', 'action' => 'show'],
 ];
-
-// return [
-//     '/api/v1/products' => '../controllers/products/index.php',
-//     '/api/v1/product' => '../controllers/products/store.php',
-//     '/api/v1/product-types' => '../controllers/productTypes/index.php',
-//     '/api/v1/product-properties' => '../controllers/productProperties/show.php',
-// ];
