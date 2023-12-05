5243645b3ed422323495a51c4396d2ece7950aac
edriso <edriso.dev@gmail.com>
2023-12-06 00:28:35 +0200

    remove a line

--- Http/Controllers/ProductTypesController.php
+++ Http/Controllers/ProductTypesController.php
@@ -5,12 +5,10 @@ namespace Http\Controllers;
 use Core\Database;
 
 class ProductTypesController {
-    public function index()
-    {
+    public function index() {
         $db = new Database();
         $products = $db->query('SELECT * FROM product_types')->get();
 
         sendJsonResponse($products);
     }
-
 }
