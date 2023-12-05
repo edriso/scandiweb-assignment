18f9e525f0f507b28d1778a485ae1b608c968fb7
edriso <edriso.dev@gmail.com>
2023-12-05 11:40:36 +0200

    complete AddProduct page structure

--- client/src/pages/AddProduct.jsx
+++ client/src/pages/AddProduct.jsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { HeaderLayout, FormRow, FormRowSelect } from '../components';
 
@@ -6,8 +6,37 @@ function AddProduct() {
   const navigate = useNavigate();
   const formRef = useRef();
 
-  const handleTypeChange = (type) => {
-    console.log(type);
+  const [productTypes, setProductTypes] = useState([]);
+
+  const [selectedType, setSelectedType] = useState({});
+  const [selectedTypeProperties, setSelectedTypeProperties] = useState([]);
+
+  const handleTypeChange = (typeId) => {
+    const selectedProductType = productTypes.find(
+      (type) => type.id === Number(typeId)
+    );
+    setSelectedType(selectedProductType);
+
+    setSelectedTypeProperties([
+      {
+        id: 1,
+        type_id: 3,
+        name: 'height',
+        unit: 'cm',
+      },
+      {
+        id: 2,
+        type_id: 3,
+        name: 'width',
+        unit: 'cm',
+      },
+      {
+        id: 3,
+        type_id: 3,
+        name: 'length',
+        unit: 'cm',
+      },
+    ]);
   };
 
   const handleSubmit = () => {
@@ -16,6 +45,29 @@ function AddProduct() {
     console.log(newProduct);
     alert('add new product');
   };
+
+  useEffect(() => {
+    setProductTypes([
+      {
+        id: 1,
+        name: 'DVD',
+        measure_name: 'size',
+        measure_unit: 'MB',
+      },
+      {
+        id: 2,
+        name: 'book',
+        measure_name: 'weight',
+        measure_unit: 'kg',
+      },
+      {
+        id: 3,
+        name: 'furniture',
+        measure_name: 'dimensions',
+        measure_unit: 'HxWxL',
+      },
+    ]);
+  }, []);
 
   return (
     <main className="add-product">
@@ -27,33 +79,42 @@ function AddProduct() {
         btnTwoAction={() => navigate('/')}
       />
 
-      <form id="product_form" className="fade-in" ref={formRef}>
+      <form method="POST" id="product_form" className="fade-in" ref={formRef}>
         <FormRow name="sku" labelText="SKU" />
         <FormRow name="name" />
         <FormRow type="number" labelText="Price ($)" name="price" />
         <FormRowSelect
           labelText="Type switcher"
           name="productType"
-          list={['Type Switcher', '1', '2', '3']}
+          placeholder="Please Select Type"
+          list={productTypes}
           onChange={(e) => handleTypeChange(e.target.value)}
         />
 
-        {/* TEMPORARY */}
-        <hr />
-        <div id="DVD" className="product-type-container">
-          <FormRow name="size" labelText="Size (MB)" />
-          <p>Please provide size in MB format</p>
-        </div>
-        <div id="Furniture" className="product-type-container">
-          <FormRow name="height" labelText="Height (CM)" />
-          <FormRow name="width" labelText="Width (CM)" />
-          <FormRow name="length" labelText="Length (CM)" />
-          <p>Please provide dimensions in HxWxL format</p>
-        </div>
-        <div id="Book" className="product-type-container">
-          <FormRow name="weight" labelText="Weight (KG)" />
-          <p>Please provide weight in KG format</p>
-        </div>
+        {!!selectedTypeProperties.length && (
+          <div id={selectedType.name} className="product-type-container">
+            {selectedTypeProperties.map((property) => {
+              return (
+                <FormRow
+                  key={property.id}
+                  name={property.name}
+                  labelText={`${property.name} (${property.unit})`}
+                />
+              );
+            })}
+            <p>
+              Please provide{' '}
+              <span className="text-capitalize">
+                {selectedType.measure_name}
+              </span>{' '}
+              in{' '}
+              <span className="text-capitalize">
+                {selectedType.measure_unit}
+              </span>{' '}
+              format
+            </p>
+          </div>
+        )}
       </form>
     </main>
   );
