e5020dfd0ca43819f70149a262c6fd29c247e9e0
edriso <edriso.dev@gmail.com>
2023-12-05 23:52:17 +0200

    require autoload

--- public/index.php
+++ public/index.php
@@ -1,14 +1,9 @@
 <?php
 
 const BASE_PATH = __DIR__.'/../';
-const CLIENT_PATH = __DIR__.'/';
-$env = parse_ini_file(BASE_PATH . '.env');
+
+require(BASE_PATH . 'vendor/autoload.php');
 
 require(BASE_PATH . 'helpers.php');
-require(BASE_PATH . 'Database.php');
+require(BASE_PATH . 'Core/Database.php');
 require(BASE_PATH . 'router.php');
-
-$db = new Database($env);
-$products = $db->query('SELECT id, sku, name, price FROM products')->get();
-// $products = $db->query('SELECT * FROM products where id = 1 limit 1')->fetch(pdo::FETCH_ASSOC);
-dd($products);
