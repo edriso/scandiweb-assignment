7f0d2718d7666923c35bc0095892e265d1217604
edriso <edriso.dev@gmail.com>
2023-12-06 00:27:59 +0200

    add abort and sendJsonResponse functions

--- helpers.php
+++ helpers.php
@@ -4,6 +4,36 @@ function base_path($path) {
     return BASE_PATH . $path;
 }
 
+function abort(
+    $errorMessage = 'Resource not found', $statusCode = 404
+) {
+    $response = [
+        'error' => [
+            'message' => $errorMessage,
+        ],
+    ];
+    
+    sendJsonResponse($response, $statusCode);
+}
+
+function sendJsonResponse($responseData, $statusCode = 200) {
+    http_response_code($statusCode);
+    header('Content-Type: application/json');
+
+    $response = [
+        'status' => $statusCode,
+    ];
+
+    if (is_array($responseData) && isset($responseData['error'])) {
+        $response['error'] = $responseData['error'];
+    } else {
+        $response['data'] = $responseData;
+    }
+
+    echo json_encode($response);
+    exit;
+}
+
 function dd($value) {
     echo "<pre>";
     var_dump($value);
