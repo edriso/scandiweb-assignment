f3251a1a22100233292235300341344b9f5d4ed3
edriso <edriso.dev@gmail.com>
2023-12-09 01:23:11 +0200

    solve CORS problem on development

--- public/index.php
+++ public/index.php
@@ -2,6 +2,11 @@
 
 const BASE_PATH = __DIR__.'/../';
 
+$config = require(BASE_PATH . 'config.php');
+if ($config['app']['env'] === 'development') {
+    header('Access-Control-Allow-Origin: *');
+}
+
 require(BASE_PATH . 'vendor/autoload.php');
 
 require(BASE_PATH . 'helpers.php');
