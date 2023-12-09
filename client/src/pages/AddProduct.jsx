012b416c7115b262ce1926e25121cc33e8b0174d
edriso <edriso.dev@gmail.com>
2023-12-09 20:09:58 +0200

    handle create new product

--- client/src/pages/AddProduct.jsx
+++ client/src/pages/AddProduct.jsx
@@ -15,23 +15,37 @@ function AddProduct() {
 
   const [selectedType, setSelectedType] = useState({});
   const [selectedTypeProperties, setSelectedTypeProperties] = useState([]);
+  const [properties, setProperties] = useState({});
 
   const handleTypeChange = async (typeId) => {
     const selectedProductType = productTypes.find(
       (type) => type.id === Number(typeId)
     );
     setSelectedType(selectedProductType);
-    const response = await apiHandler.get(`/type-properties?type_id=${typeId}`);
-    if (response.status === 200) {
+
+    try {
+      const response = await apiHandler.get(
+        `/type-properties?type_id=${typeId}`
+      );
       setSelectedTypeProperties(response.data.data);
+    } catch (error) {
+      console.log(error);
     }
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
     const formData = new FormData(formRef.current);
-    const newProduct = Object.fromEntries(formData);
-    console.log(newProduct);
-    alert('add new product');
+    const productData = {
+      ...Object.fromEntries(formData),
+      ['properties']: properties,
+    };
+
+    try {
+      await apiHandler.post('/product', JSON.stringify(productData));
+      return navigate('/');
+    } catch (error) {
+      console.log(error?.response?.data?.message);
+    }
   };
 
   return (
@@ -50,37 +64,39 @@ function AddProduct() {
         <FormRow type="number" labelText="Price ($)" name="price" />
         <FormRowSelect
           labelText="Type switcher"
-          name="productType"
+          name="type_id"
           placeholder="Please Select Type"
           list={productTypes}
           onChange={(e) => handleTypeChange(e.target.value)}
         />
-
-        {!!selectedTypeProperties.length && (
-          <div id={selectedType.name} className="product-type-container">
-            {selectedTypeProperties.map((property) => {
-              return (
-                <FormRow
-                  key={property.id}
-                  name={property.name}
-                  labelText={`${property.name} (${property.unit})`}
-                />
-              );
-            })}
-            <p>
-              Please provide{' '}
-              <span className="text-capitalize">
-                {selectedType.measure_name}
-              </span>{' '}
-              in{' '}
-              <span className="text-capitalize">
-                {selectedType.measure_unit}
-              </span>{' '}
-              format
-            </p>
-          </div>
-        )}
       </form>
+
+      {!!selectedTypeProperties.length && (
+        <div id={selectedType.name} className="product-type-container">
+          {selectedTypeProperties.map((property) => {
+            return (
+              <FormRow
+                key={property.id}
+                name={property.name}
+                labelText={`${property.name} (${property.unit})`}
+                onChange={(e) =>
+                  setProperties({
+                    ...properties,
+                    [property.name]: e.target.value,
+                  })
+                }
+              />
+            );
+          })}
+          <p>
+            Please provide{' '}
+            <span className="text-capitalize">{selectedType.measure_name}</span>{' '}
+            in{' '}
+            <span className="text-capitalize">{selectedType.measure_unit}</span>{' '}
+            format
+          </p>
+        </div>
+      )}
     </main>
   );
 }
