8c689886c705ed5535c28792b0a5e7b596c065bc
edriso <edriso.dev@gmail.com>
2023-12-10 21:06:28 +0200

    solve CORS problem on development

--- public/index.php
+++ public/index.php
@@ -1,12 +1,19 @@
 <?php
 
-const BASE_PATH = __DIR__.'/../';
+header('Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS');
 
+const BASE_PATH = __DIR__.'/../';
 $config = require(BASE_PATH . 'config.php');
+
 if ($config['app']['env'] === 'development') {
     header('Access-Control-Allow-Origin: *');
+
+    // If the request is an OPTIONS request (pre-flight check)
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        header("HTTP/1.1 200 OK");
+        exit;
+    }
 }
-header('Access-Control-Allow-Methods: GET, POST, DELETE');
 
 require(BASE_PATH . 'vendor/autoload.php');
 require(BASE_PATH . 'Core/helpers.php');
@@ -16,4 +23,5 @@ $routes = require(BASE_PATH . 'routes.php');
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
+
 $router->route($uri, $method);
