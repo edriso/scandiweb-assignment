d3465a0666d24b6bf5c156725ade62342891b799
edriso <edriso.dev@gmail.com>
2023-12-12 13:44:52 +0200

    setup react-toastify

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './assets/scss/main.scss';
+import 'react-toastify/dist/ReactToastify.css';
+import { Slide, ToastContainer } from 'react-toastify';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <ToastContainer position="top-right" transition={Slide} autoClose={2000} />
   </React.StrictMode>
 );
