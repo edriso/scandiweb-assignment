43bacd6014b199cd6bc34e0440467f3fae249305
edriso <edriso.dev@gmail.com>
2023-12-15 18:06:41 +0200

    separate file for product validation

--- Http/Models/Product.php
+++ Http/Models/Product.php
@@ -5,7 +5,7 @@ namespace Http\Models;
 use Core\Database;
 use Core\Response;
 use ReflectionClass;
-use Http\Models\Classes\PropertyType;
+use Http\Validations\ProductValidation;
 
 abstract class Product {
     protected $sku;
@@ -15,7 +15,7 @@ abstract class Product {
     protected $properties;
 
     public function __construct($attributes) {
-        $this->validateMandatoryFields($attributes);
+        ProductValidation::validateMandatoryFields($attributes);
 
         $this->sku = $attributes['sku'];
         $this->name = $attributes['name'];
@@ -23,85 +23,11 @@ abstract class Product {
         $this->typeId = $attributes['type_id'];
         $this->properties = $attributes['properties'] ?? [];
 
-        $this->validateProperties($this->properties);
-    }
-
-    protected function getInstanceTypeName() {
-        return (new ReflectionClass($this))->getShortName();
-    }
-
-    protected function validateMandatoryFields($fields) {
-        $mandatoryFields = ['sku', 'name', 'price'];
-
-        foreach ($mandatoryFields as $field) {
-            if (!isset($fields[$field]) || empty($fields[$field])) {
-                Response::abort("Missing or empty field: $field", 400);
-            }
-
-            switch ($field) {
-                case 'price':
-                    if (!is_numeric($fields[$field]) || $fields[$field] < 0) {
-                        Response::abort("Invalid data type for $field. Must be a non-negative number.", 400);
-                    }
-                    break;
-                case 'name':
-                    if (!is_string($fields[$field]) || is_numeric($fields[$field])) {
-                        Response::abort("Invalid data type for $field. Must be a string.", 400);
-                    }
-                    break;
-                case 'sku':
-                    if (!ctype_alnum($fields[$field])) {
-                        Response::abort("Invalid value for $field. Must be alphanumeric.", 400);
-                    }
-                    break;
-            }
-        }
-    }
-
-    protected function validateProperties($properties) {
-        $instanceTypeName = $this->getInstanceTypeName();
-        $expectedProperties = $this->getProperties();
-
-        if (!is_array($properties) || count($properties) !== count($expectedProperties)) {
-            Response::abort("{$instanceTypeName} properties must include: " . implode(', ', array_keys($expectedProperties)), 400);
-        }
-
-        foreach ($expectedProperties as $propName => $propType) {
-            if (!isset($properties[$propName])) {
-                Response::abort("Missing property: $propName", 400);
-            }
-
-            if (!$this->isValidType($properties[$propName], $propType)) {
-                Response::abort("Invalid data type for property $propName. Expected: $propType", 400);
-            }
-        }
-    }
-
-    protected function isValidType($value, $expectedType) {
-        if ($expectedType === PropertyType::NUMERIC) {
-            return is_numeric($value);
-        } elseif ($expectedType === PropertyType::STRING) {
-            return is_string($value);
-        } else {
-            return false;
-        }
-    }
-
-    protected static function getTypeName($typeId) {
-        $db = new Database();
-        $query = 'SELECT name FROM product_types WHERE id = ?';
-        $productType = $db->query($query, [$typeId])->getOneOrFail();
-        return ucfirst($productType['name']);
-    }
-
-    protected static function areValidIds(array $ids) {
-        foreach ($ids as $id) {
-            if (!is_numeric($id) || $id <= 0) {
-                return false;
-            }
-        }
-    
-        return true;
+        ProductValidation::validateProperties(
+            $this->properties,
+            $this->getInstanceTypeName(),
+            $this->getProperties()
+        );
     }
 
     public static function getAll() {
@@ -110,18 +36,16 @@ abstract class Product {
                   FROM products p
                   JOIN product_types t ON p.type_id = t.id
                   ORDER BY p.id DESC';
-        
         $productsData = $db->query($query)->get();
-    
+
         $products = [];
-        
         foreach ($productsData as $productData) {
-            $productType = 'Http\Models\Classes\ProductTypes\\' . ucfirst($productData['type']);
+            $productType = 'Http\Models\ProductTypes\\' . ucfirst($productData['type']);
             $productData['description'] = $productType::description(json_decode($productData['properties'], true));
             unset($productData['properties'], $productData['type_id']);
             $products[] = $productData;
         }
-    
+
         return $products;
     }
 
@@ -130,7 +54,7 @@ abstract class Product {
             Response::abort('Missing type_id', 400);
         }
 
-        $productType = 'Http\Models\Classes\ProductTypes\\' . self::getTypeName($attributes['type_id']);
+        $productType = 'Http\Models\ProductTypes\\' . self::getTypeName($attributes['type_id']);
 
         $productInstance = new $productType($attributes);
 
@@ -150,15 +74,23 @@ abstract class Product {
     }
 
     public static function deleteByIds(array $productIds) {
-        if (!self::areValidIds($productIds)) {
-            Response::abort('Invalid product(s) ID.', 400);
-        }
+        ProductValidation::validateIds($productIds);
 
         $db = new Database();
         $placeholders = implode(',', array_fill(0, count($productIds), '?'));
-
         $query = "DELETE FROM products WHERE id IN ($placeholders)";
         $db->query($query, $productIds);
+    }
+
+    protected function getInstanceTypeName() {
+        return (new ReflectionClass($this))->getShortName();
+    }
+
+    protected static function getTypeName($typeId) {
+        $db = new Database();
+        $query = 'SELECT name FROM product_types WHERE id = ?';
+        $productType = $db->query($query, [$typeId])->getOneOrFail();
+        return ucfirst($productType['name']);
     }
 
     abstract protected function getProperties(): array;
